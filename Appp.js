dce588fc805c870231936368db21e48b625bf687
Xiaowen Fang <164426683@qq.com>
2017-05-06 22:06:34 -0400

    Delete Appp.js

--- Appp.js
+++
@@ -1,9 +0,0 @@
-var React = require('react'),
-    DOM = React.DOM, div = DOM.div;
-
-module.exports = React.createClass({
-    render: function(){
-        var content = 'Hello World';
-        return div(null, content);
-    }
-})
